6f86c6642c7063573c19f73d09aab33b9f6d48c2
igl-myrick <igl.myrick@gmail.com>
2024-05-07 13:51:01 -0700

    add test for venusAge property

--- __tests__/converted-age.test.js
+++ __tests__/converted-age.test.js
@@ -1,9 +1,17 @@
 import ConvertedAge from '../src/converted-age.js';
 
 describe("ConvertedAge", () => {
+  let newConversion;
+
+  beforeEach(() => {
+    newConversion = new ConvertedAge(20);
+  });
 
   test("should store a new class with a mercuryAge value", () => {
-    let newConversion = new ConvertedAge(20);
     expect(newConversion.mercuryAge).toEqual(83.333);
+  });
+
+  test("should store a class with an additional venusAge value", () => {
+    expect(newConversion.venusAge).toEqual(32.258);
   });
 });
