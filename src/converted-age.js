260dc12e866976ef55b7c4b0e138f8517250686d
igl-myrick <igl.myrick@gmail.com>
2024-05-07 14:11:50 -0700

    add jupiterAge property

--- src/converted-age.js
+++ src/converted-age.js
@@ -3,5 +3,6 @@ export default class ConvertedAge {
     this.mercuryAge = parseFloat((inputAge / 0.24).toFixed(3));
     this.venusAge = parseFloat((inputAge / 0.62).toFixed(3));
     this.marsAge = parseFloat((inputAge / 1.88).toFixed(3));
+    this.jupiterAge = parseFloat((inputAge / 11.86).toFixed(3));
   }
 }
