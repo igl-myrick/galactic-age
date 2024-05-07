a6db454c9ce56cde3c280aa9f9e4a2cb2380b2f4
igl-myrick <igl.myrick@gmail.com>
2024-05-07 15:21:49 -0700

    add getAge prototype

--- src/converted-age.js
+++ src/converted-age.js
@@ -1,5 +1,6 @@
 export default class ConvertedAge {
   constructor(inputAge) {
+    this.inputAge = inputAge;
     this.mercuryFormula = function(inputNum) {
       return parseFloat((inputNum / 0.24).toFixed(3));
     }
@@ -11,6 +12,14 @@ export default class ConvertedAge {
     }
     this.jupiterFormula = function(inputNum) {
       return parseFloat((inputNum / 11.86).toFixed(3));
+    }
+  }
+
+  getAge(planetToConvert) {
+    if (planetToConvert.toLowerCase() === "mercury") {
+      return this.mercuryFormula(this.inputAge);
+    } else {
+      return "Invalid input";
     }
   }
 }
