e47d7e2a5ee3d423e846cd6f0f14ac8bad758d3e
igl-myrick <igl.myrick@gmail.com>
2024-05-07 13:52:49 -0700

    add venusAge property

--- src/converted-age.js
+++ src/converted-age.js
@@ -1,5 +1,6 @@
 export default class ConvertedAge {
   constructor(inputAge) {
     this.mercuryAge = parseFloat((inputAge / 0.24).toFixed(3));
+    this.venusAge = parseFloat((inputAge / 0.62).toFixed(3));
   }
 }
