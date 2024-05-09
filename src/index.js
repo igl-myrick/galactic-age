556399a4cde1a7512dcc1a2fcc4c2bce0fe9c0f6
igl-myrick <igl.myrick@gmail.com>
2024-05-09 16:02:42 -0700

    add error handling for handleSinceBirthdayForm

--- src/index.js
+++ src/index.js
@@ -10,6 +10,14 @@ function clearResult(element) {
   }
 }
 
+function displayError(elementToAppend, errorText, errorID) {
+  const errorP = document.createElement("p");
+  errorP.setAttribute("id", errorID);
+  errorP.innerText = errorText;
+  errorP.style.color = "red";
+  elementToAppend.append(errorP);
+}
+
 function handleConversionForm(e) {
   e.preventDefault();
   clearResult("p#conversion-result-p");
@@ -19,9 +27,14 @@ function handleConversionForm(e) {
 
   const userAge = document.getElementById("user-age-1").value;
   const userPlanet = document.getElementById("planet-selection").value;
-  const newConversion = new ConvertedAge(userAge);
-  newP.innerText = `Your age on ${userPlanet} would be ${newConversion.getAge(userPlanet)}.`;
-  conversionDiv.append(newP);
+  if (userAge <= 0) {
+    displayError(conversionDiv, "Please enter a positive number.", "conversion-error");
+  } else {
+    clearResult("p#conversion-error");
+    const newConversion = new ConvertedAge(userAge);
+    newP.innerText = `Your age on ${userPlanet} would be ${newConversion.getAge(userPlanet)}.`;
+    conversionDiv.append(newP);
+  }
 }
 
 function handleSinceBirthdayForm(e) {
@@ -31,11 +44,18 @@ function handleSinceBirthdayForm(e) {
   newP.setAttribute("id", "since-birthday-result-p");
   const sinceBirthdayDiv = document.getElementById("since-birthday-result");
 
-  const userCurrentAge = document.getElementById("user-age-2").value;
-  const userPastAge = document.getElementById("past-age").value;
-  const newConversion = new ConvertedAge(userCurrentAge);
-  newP.innerText = newConversion.sinceBirthday(userPastAge);
-  sinceBirthdayDiv.append(newP);
+  const userCurrentAge = parseInt(document.getElementById("user-age-2").value);
+  const userPastAge = parseInt(document.getElementById("past-age").value);
+  if ((userCurrentAge <= 0 || userPastAge <= 0) || (userCurrentAge <= 0 && userPastAge <= 0)) {
+    displayError(sinceBirthdayDiv, "Please enter a positive number.", "since-birthday-error");
+  } else if (userPastAge >= userCurrentAge) {
+    displayError(sinceBirthdayDiv, "The second number must be lower than the first.", "since-birthday-error");
+  } else {
+    clearResult("p#since-birthday-error");
+    const newConversion = new ConvertedAge(userCurrentAge);
+    newP.innerText = newConversion.sinceBirthday(userPastAge);
+    sinceBirthdayDiv.append(newP);
+  }
 }
 
 function handleUntilBirthdayForm(e) {
