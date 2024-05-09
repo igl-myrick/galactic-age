4f86835f3890c28b710c4e581e548456d00353b2
igl-myrick <igl.myrick@gmail.com>
2024-05-09 14:29:42 -0700

    add handling for sinceBirthday method display

--- src/index.js
+++ src/index.js
@@ -5,19 +5,37 @@ import ConvertedAge from "./converted-age.js";
 
 function handleConversionForm(e) {
   e.preventDefault();
-  if (document.querySelector("p#converted-age-result")) {
-    const pastResult = document.querySelector("p#converted-age-result");
+  if (document.querySelector("p#conversion-result-p")) {
+    const pastResult = document.querySelector("p#conversion-result-p");
     pastResult.remove();
   }
   const newP = document.createElement("p");
-  newP.setAttribute("id", "converted-age-result");
-  const conversionDiv = document.getElementById("left");
+  newP.setAttribute("id", "conversion-result-p");
+  const conversionDiv = document.getElementById("conversion-result");
 
-  const userAge = document.getElementById("user-age").value;
+  const userAge = document.getElementById("user-age-1").value;
   const userPlanet = document.getElementById("planet-selection").value;
   const newConversion = new ConvertedAge(userAge);
   newP.innerText = `Your age on ${userPlanet} would be ${newConversion.getAge(userPlanet)}.`;
   conversionDiv.append(newP);
 }
 
+function handleSinceBirthdayForm(e) {
+  e.preventDefault();
+  if (document.querySelector("p#since-birthday-result-p")) {
+    const pastResult = document.querySelector("p#since-birthday-result-p");
+    pastResult.remove();
+  }
+  const newP = document.createElement("p");
+  newP.setAttribute("id", "since-birthday-result-p");
+  const sinceBirthdayDiv = document.getElementById("since-birthday-result");
+
+  const userCurrentAge = document.getElementById("user-age-2").value;
+  const userPastAge = document.getElementById("past-age").value;
+  const newConversion = new ConvertedAge(userCurrentAge);
+  newP.innerText = newConversion.sinceBirthday(userPastAge);
+  sinceBirthdayDiv.append(newP);
+}
+
 document.getElementById("age-conversion").addEventListener("submit", handleConversionForm);
+document.getElementById("since-birthday").addEventListener("submit", handleSinceBirthdayForm);
